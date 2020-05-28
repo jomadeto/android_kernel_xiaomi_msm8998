d17fa0b8b8ff513197d85cfb0c737c689b2d2091
aa889788 <shxyke@126.com>
2020-05-28 08:00:50 -0400

    mdss: Introduce DC Dimming
    * With changes from
      https://github.com/DerpFest-Devices/lazy_kernel_oneplus_msm8998/commits/11/drivers/video
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -28,6 +28,10 @@
 #include "mdss_debug.h"
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_FLICKER_FREE
+#include "flicker_free.h"
+#endif
+
 #include <linux/clk.h>
 #include <linux/project_info.h>
 #if defined(CONFIG_IRIS2P_FULL_SUPPORT)
@@ -1119,6 +1123,12 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 
 	if ((bl_level < pdata->panel_info.bl_min) && (bl_level != 0))
 		bl_level = pdata->panel_info.bl_min;
+
+#ifdef CONFIG_FLICKER_FREE
+	/* remap backlight value */
+	if (bl_level != 0)
+		bl_level = mdss_panel_calc_backlight(bl_level);
+#endif
 
 	/* enable the backlight gpio if present */
 	mdss_dsi_bl_gpio_ctrl(pdata, bl_level);
